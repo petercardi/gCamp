e6d34cd7f9d8ce29a118258c4fdd82fb0cee546d
Peter Cardi <peter.v.cardi@gmail.com>
2015-02-21 00:34:05 -0700

    users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email)
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_path, notice: "User was successfully created"
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    user_params = params.require(:user).permit(:first_name, :last_name, :email)
+    @user.update(user_params)
+    redirect_to users_path, notice: "User was successfully updated"
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, notice: "User was successfully deleted"
+  end
+
+end
