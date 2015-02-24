572a4c8d92e7e619bb5a67d5386776fd95f82918
Peter Cardi <peter.v.cardi@gmail.com>
2015-02-24 14:01:41 -0700

    projects . . . sort of

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,38 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    project_params = params.require(:project).permit(:name)
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to projects_path, notice: "Project was successfully created"
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    project_params = params.require(:project).permit(:name)
+    @project.update(project_params)
+    redirect_to projects_path, notice: "Project was successfully updated"
+  end
+
+  def destroy
+    Project.find(params[:id]).destroy
+    redirect_to project_path, notice: "Project was successfully deleted"
+  end
+
+end
