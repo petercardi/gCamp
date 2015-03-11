c011b01f611e65e7de105e02d8505309a4d00a43
Peter Cardi <peter.v.cardi@gmail.com>
2015-03-11 12:18:19 -0600

    removed back button

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @plugs = {
-      '"gCamp has changed my life! It\'s the best tool I\'ve ever used."' => '- Cayla Hayes', 
+      '"gCamp has changed my life! It\'s the best tool I\'ve ever used."' => '- Cayla Hayes',
       '"Before gCamp I was a disorderly slob.  Now I\'m more organized than I\'ve ever been"' => "- Leta Jaskolski",
-      '"Don\'t hesitate - sign up right now! You\'ll never be the same."' =>"- Lavern Upton"
+      '"Don\'t hesitate - sign up right now! You\'ll never be the same."' =>"- Laverne Upton"
       }
   end
 end
